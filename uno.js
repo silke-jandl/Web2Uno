55e07a893b66cc1e101f6c50f0ef641732c6a697
Silke Jandl <s.jandl@wealthpilot.de>
2020-12-03 15:01:13 +0100

    - embedded api so we can work with it
    - also included the file with all the card images
    - first html structure attempt

---
+++ uno.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let names = ["name1", "name2", "name3", "name4"]
+async function post(){
+    let response = await fetch("https://nowaunoweb.azurewebsites.net/api/game/start", {
+        method: "POST",
+        body: JSON.stringify(names),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    });
+
+    if(response.ok){
+        let result = await response.json();
+        alert(JSON.stringify(result))
+    }
+    else {
+        alert("HTTP-Error: "  + response.status)
+    }
+}
+post();
